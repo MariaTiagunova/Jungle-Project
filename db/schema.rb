6869b7bc439717d879828c961eb6d4fd10a1a137
MariaTiagunova <tiagunova.maria@gmail.com>
2023-11-07 16:29:52 -0500

    Add Rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_26_180148) do
+ActiveRecord::Schema.define(version: 2021_06_25_110200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,24 +21,11 @@ ActiveRecord::Schema.define(version: 2023_10_26_180148) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "line_items", force: :cascade do |t|
-    t.bigint "order_id"
-    t.bigint "product_id"
-    t.integer "quantity"
-    t.integer "item_price_cents"
-    t.integer "total_price_cents"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_line_items_on_order_id"
-    t.index ["product_id"], name: "index_line_items_on_product_id"
-  end
-
   create_table "orders", force: :cascade do |t|
     t.integer "total_cents"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "stripe_charge_id"
-    t.string "email"
   end
 
   create_table "products", force: :cascade do |t|
@@ -53,15 +40,5 @@ ActiveRecord::Schema.define(version: 2023_10_26_180148) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "line_items", "orders"
-  add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
 end
